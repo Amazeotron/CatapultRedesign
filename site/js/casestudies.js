94396c7c9fc400837e8c25ce59d792505db66336
Larry Lague <sayhitolarry@gmail.com>
2012-09-19 22:40:11 -0700

    more change to homepage

--- site/js/casestudies.js
+++ site/js/casestudies.js
@@ -3,52 +3,74 @@ var casestudies = {
 
 	_data: null,
 	_activeIndex: 0,
+	_lastIndex: 0,
 
 	/*
 	* Kick off the Case Studies. Pass in data array that represents the case studies.
 	*/
 	init: function(data) {
 		this._data = data;
-		this.showCaseStudy(this._activeIndex);
+		this.showCaseStudy(this._activeIndex, true);
 
 		var self = this;
 
 		$("#casestudy-paddle-prev").click(function(event) {
 			event.preventDefault();
-			self.showCaseStudy(self.getPrevIndex());
+			self.showCaseStudy(self.getPrevIndex(), false);
 			return false;
 		});
 		$("#casestudy-paddle-next").click(function(event) {
 			event.preventDefault();
-			self.showCaseStudy(self.getNextIndex());
+			self.showCaseStudy(self.getNextIndex(), false);
 			return false;
+		});
+
+		//Key Offerings buttons
+		$(".keyofferings-list-item a").click(function(event) {
+			event.preventDefault();
+			$(".keyofferings-list-item a").removeClass("active");
+			$(this).addClass("active");
+			$(".casestudies").removeClass("engineering ideation innovation education insights systhink");
+			$(".casestudies").addClass($(this).attr("class"));
 		});
 	}, 
 
-	showCaseStudy: function(index) {
+	showCaseStudy: function(index, firstTime) {
 		var study = this._data[index],
-		container = $("#casestudy-content-sub");
+		container = $("#casestudy-content-sub"),
+		imgContainer = $("#casestudy-images-sub"),
 		html = '<div class="casestudy-content-single" id="' + study.id + '">' +
 						'<img src="' + study.headerImg + '" alt="' + study.title + '" class="casestudy-content-header" />' +
 						'<h1>' + study.title + '</h1>' +
 						'<p class="casestudy-body">' + study.body + '</p>' +
-						'</div>';
+						'</div>',
+		self = this;
 
-		// container.empty();
-		container.append(html);
-
-		var offset = index*300+20;
+		var bodyOffset = index*393+20, 
+		imgId = 'IMG-'+study.id,
+		imgOffset = index*900;
 		
-		$("#"+study.id).css({left:offset});
-		$(".casestudy-images").css({"background-image":"url(" + study.mainImg + ")"});
-
 		// animate the container
-		container.animate({
-			left: -offset
-		}, 500);
+		container.append(html);
+		$("#"+study.id).css({left:bodyOffset});
+		container.css({left:-bodyOffset});
+
+		// Animate the large image
+		imgContainer.append('<img src="' + study.mainImg + '" id="' + imgId + '" />');
+		$("#"+imgId).css({left:imgOffset});
+		imgContainer.css({left:-imgOffset});
+
+		// Remove old content after delay
+		if (firstTime === false) {
+			setTimeout(function() {
+				var lastId = "#"+self._data[self._lastIndex].id;
+				$(lastId).remove();
+			}, 500);
+		}
 	}, 
 
 	getNextIndex: function() {
+		this._lastIndex = this._activeIndex;
 		this._activeIndex++;
 		if (this._activeIndex >= this._data.length) this._activeIndex = 0;
 		console.log("active case study index: " + this._activeIndex);
@@ -56,6 +78,7 @@ var casestudies = {
 	},
 
 	getPrevIndex: function() {
+		this._lastIndex = this._activeIndex;
 		this._activeIndex--;
 		if (this._activeIndex < 0) this._activeIndex = this._data.length - 1;
 		console.log("active case study index: " + this._activeIndex);
--- site/js/main.js
+++ site/js/main.js
@@ -15,11 +15,39 @@ $(document).ready(function() {
 			"body": "<p>In resource-strapped Tanzania, farmers and their families spend an average of 5 hours a day carrying heavy loads of water, firewood, and food to their homes or market.</p><p>Anza Technologies was working to create an affordable yet durable handcart for rural communities. They had some ideas - including wheels made of inflated 2 liter bottles - but needed a scalable solution suited for a rugged environment.</p><p>Over the course of 7 months, Catapult stepped in to provide user research, competitive analysis, design, prototyping, CAD development, and user testing.</p><p>Beginning with a competitive analysis of the handcart market, the Catapult team found there was a distinct gap between carts sold in stores with an upwards cost of $100 USD, and homemade carts which resembled wooden sleighs.</p><p>The team began designing a handcart better suited to our customers’ needs. Three designs were developed that would pack flat, ship easily, and retain their durability. These prototypes were sent to Tanzania along with Noel and Tyler. Working with the Matala community the field prototypes were tested, revised, reviewed and improved.</p><p>The result was a $20 USD hand cart capable of carrying 300lb over long, rugged paths, and maintained within the local community.</p>"
 		},
 		{
-			"id": "sample",
+			"id": "sample1",
 			"mainImg": "img/casestudies/handcart/IMG_0487.jpg",
 			"headerImg": "img/casestudies/handcart/caption.jpg",
-			"title": "ANOTHER HEADLINE",
-			"body": "<p>More body copy...</p>"
+			"title": "ANOTHER HEADLINE 1",
+			"body": "<p>More body copy... 1</p>"
+		},
+		{
+			"id": "sample2",
+			"mainImg": "img/casestudies/handcart/IMG_0487.jpg",
+			"headerImg": "img/casestudies/handcart/caption.jpg",
+			"title": "ANOTHER HEADLINE 2",
+			"body": "<p>More body copy... 2</p>"
+		},
+		{
+			"id": "sample3",
+			"mainImg": "img/casestudies/handcart/IMG_0487.jpg",
+			"headerImg": "img/casestudies/handcart/caption.jpg",
+			"title": "ANOTHER HEADLINE 3",
+			"body": "<p>More body copy... 3</p>"
+		},
+		{
+			"id": "sample4",
+			"mainImg": "img/casestudies/handcart/IMG_0487.jpg",
+			"headerImg": "img/casestudies/handcart/caption.jpg",
+			"title": "ANOTHER HEADLINE 4",
+			"body": "<p>More body copy... 4</p>"
+		},
+		{
+			"id": "sample5",
+			"mainImg": "img/casestudies/handcart/IMG_0487.jpg",
+			"headerImg": "img/casestudies/handcart/caption.jpg",
+			"title": "ANOTHER HEADLINE 5",
+			"body": "<p>More body copy... 5</p>"
 		}
 	];
 	casestudies.init(studies);
