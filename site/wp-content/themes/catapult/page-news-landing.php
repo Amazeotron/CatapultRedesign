833867730a9715d08c754574fa117ad38fcf3085
Larry Lague <sayhitolarry@gmail.com>
2013-08-12 22:19:39 -0700

    More updates to new News page.

--- site/wp-content/themes/catapult/page-news-landing.php
+++ site/wp-content/themes/catapult/page-news-landing.php
@@ -34,48 +34,37 @@
 			?>
 			
 			<?php foreach ($newsItems as $newsItem) : ?>
-					<?php if ($newsItem->type != "twitter") :
-						$headerImage = $newsItem->header_image_id;
-						$croppedImage;
-						if ($headerImage) : 
-							$croppedImage = wp_get_attachment_image_src($headerImage, array(643, 183));
-							$croppedImage = $croppedImage[0]; ?>
-						<article class="post-box">
-						<figure>
-							<img class="post-image" src="<?php echo $croppedImage ?>" alt="<?php echo $newsItem->title; ?>" width="100%">
-						</figure>
-							
-						<? else : ?>
-						<article class="post-box with-bg">
-						<? endif; ?>
-						
-						<header class="post-top">
-							<h1 class="post-title header-title"><a href="<?php echo $newsItem->link; ?>"><?php echo $newsItem->title; ?></a></h1>
-
-              <?php if ($newsItem->type != "twitter") : ?>
-                <div class="post-meta">
-                  <span>By <?php echo $newsItem->author; ?></span> | <span><?php echo $newsItem->nice_date; ?></span>
-                </div>
-              <? endif; ?>
-              
-              <?php echo $newsItem->excerpt; ?>
-              
-							<ul class="tags">
-		        <?php
-		        $categories = $newsItem->categories;
-		        foreach($categories as $cat) : ?>
-		        	<li class="tag header-title"><a class="category-item <? echo $cat['slug']; ?>" href="<? echo get_category_link($cat['id']) ?>"><? echo $cat['slug']; ?></a></li>
-		        <? endforeach; ?>
-	    		</ul>
-	    		<? else : ?>
-	    		<header class="post-top twitter">
-						<p class="post-tweet"><?php echo $newsItem->title; ?></p>
-						<span class="category-item header-title insights">TWEET</span> <a class="post-tweetlink" href="<?php echo $newsItem->link ?>">@catapult_design</a> <span class="post-tweetdate">| <?php echo $newsItem->nice_date; ?></span>
-					<? endif; ?>
-
-				</header><!-- end post-top -->
-					
-				</article>
+        <article class="post-box with-bg">
+      <?php 
+      $headerImage = $newsItem->header_image_id;
+      $croppedImage = '';
+      if ($headerImage) : 
+        $croppedImage = wp_get_attachment_image_src($headerImage, 'thumbnail');
+        $croppedImage = $croppedImage[0]; ?>
+          <figure>
+            <img class="post-image" src="<?php echo $croppedImage ?>" alt="<?php echo $newsItem->title; ?>" width="100%">
+          </figure>
+      <? endif; ?>    
+          <header class="post-top <?php if (!$headerImage) { echo "full-width"; } ?>">
+            <h1 class="post-title header-title"><a href="<?php echo $newsItem->link; ?>"><?php echo $newsItem->title; ?></a></h1>
+  
+            <?php if ($newsItem->type != "twitter") : ?>
+              <div class="post-meta">
+                <span>By <?php echo $newsItem->author; ?></span> | <span><?php echo $newsItem->nice_date; ?></span>
+              </div>
+            <? endif; ?>
+            
+            <?php echo $newsItem->excerpt; ?>
+            
+            <ul class="tags">
+          <?php
+          $categories = $newsItem->categories;
+          foreach($categories as $cat) : ?>
+            <li class="tag header-title"><a class="category-item <? echo $cat['slug']; ?>" href="<? echo get_category_link($cat['id']) ?>"><? echo $cat['slug']; ?></a></li>
+          <? endforeach; ?>
+            </ul>
+          </header><!-- end post-top -->
+        </article>
 			<?php endforeach; ?>
 			
 			</div><!-- end posts -->
--- site/wp-content/themes/catapult/sidebar.php
+++ site/wp-content/themes/catapult/sidebar.php
@@ -1,81 +1,74 @@
 <?php
-$categories = (array) get_the_category();
+$categories = (array)get_the_category();
 $catNames = array();
 $catIDs = array();
-foreach($categories as $category) {
-    array_push($catNames, $category->category_nicename);
-    array_push($catIDs, $category->category_id);
+foreach ($categories as $category) {
+  array_push($catNames, $category->category_nicename);
+  array_push($catIDs, $category->category_id);
 }
 $catIDsSeparated = implode(",", $catIDs);
 
 ?>
 <aside id="main-aside">
-    <div id="main-aside-inner">
-        <div class="divider"></div>
-        <ul class="tags">
-            <?php
-            foreach($categories as $cat) {
-                echo '<li class="tag header-title"><a class="category-item ' . $cat->category_nicename . '" href="' . get_category_link($cat->term_id) . '">' . $cat->category_nicename . '</a></li>';
-            }
-            ?>
-        </ul>
-        
-        <?php 
-        $args = array(
-            'numberposts' => 5, 
-            'offset' => 0, 
-            'category' => $catIDsSeparated,
-            'orderby' => 'post_date',
-            'post_type' => 'post',
-            'exclude' => get_the_ID()
-        );
-        $posts_array = get_posts( $args );
-        ?>
-        <h3 class="header-title related-header">Related Articles</h3>
-        <ul class="related-articles">
-            <?php foreach( $posts_array as $post ) :  setup_postdata($post); ?>
-            <li class="related-article header-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-        
-    
-    <div class="contact-buttons">
-        <a href="http://oi.vresp.com/?fid=9fcbd66d02" title="Subscribe to Newsletter" class="header-title contact-button contact-button--newsletter">Subscribe to Newsletter</a>
-        
-        <a href="<?php echo home_url('/'); ?>?feed=rss2" class="header-title contact-button contact-button--rss">Subscribe to Blog</a>
-    </div>
-    
-    <!-- Twitter Feed -->
+  <div id="main-aside-inner">
+    <hr/>
+    <ul class="tags">
+      <?php
+      foreach ($categories as $cat) {
+        echo '<li class="tag header-title"><a class="category-item ' . $cat->category_nicename . '" href="' . get_category_link($cat->term_id) . '">' . $cat->category_nicename . '</a></li>';
+      }
+      ?>
+    </ul>
+
     <?php
-    // Pull in XML feed for Twitter posts
-    include(ABSPATH . "wp-content/themes/catapult/inc/TwitterRSSParser.php");
-    $twitterParser = new TwitterRSSParser();
-    $tweets = NewsParser::commonize($twitterParser->init(), "twitter", 4);
-    echo '<ul class="twitter-list">';
-    foreach ($tweets as $newsItem) {
-      echo '<li class="twitter-list__item">' . $newsItem->title . "</li>";
-    }
-    echo "</ul>";
-
-    // Get the Twitter feed for the sidebar
-
-    function getTwitterFeed($username) {
-      include_once('inc/TwitterAPIExchange.php');
-
-      $settings = array(
-        'oauth_access_token' => "463123231-fjumJz5441JSO6OAVWbMxhNOu56mFYjwU0hv9p5c",
-        'oauth_access_token_secret' => "vIxoEmf6hvvp8cjQJiDZNPTQWN6ou0tXUKNXAwuJ2TI",
-        'consumer_key' => "vXiVeH1ut0pmml9JhbhHVw",
-        'consumer_secret' => "zAoHQN3Tlpcb9utdjReMPapkqMwYpRMwUPkaKFicws"
-      );
-      $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-      $getfield = '?screen_name=' . $username . '&include_entities=true&include_rts=true&count=5';
-      $requestMethod = 'GET';
-      $twitter = new TwitterAPIExchange($settings);
-      $twitter->setGetfield($getfield);
-      $twitter->buildOauth($url, $requestMethod);
-      return json_decode($twitter->performRequest());
-    }
+    $args = array(
+      'numberposts' => 5,
+      'offset' => 0,
+      'category' => $catIDsSeparated,
+      'orderby' => 'post_date',
+      'post_type' => 'post',
+      'exclude' => get_the_ID()
+    );
+    $posts_array = get_posts($args);
     ?>
+    <h3 class="header-title related-header">Related Articles</h3>
+    <ul class="related-articles">
+      <?php foreach ($posts_array as $post) :  setup_postdata($post); ?>
+        <li class="related-article header-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <hr/>
+  <?php
+  // Twitter Feed
+  function getTwitterFeed($username)
+  {
+    include_once(ABSPATH . 'wp-content/themes/catapult/inc/TwitterAPIExchange.php');
+
+    $settings = array(
+      'oauth_access_token' => "44148814-WlXoeq49W90zNYIpgSxK5NIYs3milWoWFVeigIonc",
+      'oauth_access_token_secret' => "Dsa93Sax5wBgyuax97YMyBdYW0H2mHInq4VmfZPeHX8",
+      'consumer_key' => "0uFBpTicI4ol9l75AQVw",
+      'consumer_secret' => "1IZAXGlCEI1wV6oixiL8TRAg692hj8QK4sEEhZrw4"
+    );
+    $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+    $getfield = '?screen_name=' . $username . '&include_entities=true&include_rts=true&count=5';
+    $requestMethod = 'GET';
+    $twitter = new TwitterAPIExchange($settings);
+    $twitter->setGetfield($getfield);
+    $twitter->buildOauth($url, $requestMethod);
+    return json_decode($twitter->performRequest());
+  }
+
+  echo '<ul class="twitter-list">';
+  foreach (getTwitterFeed('catapult_design') as $tweet) {
+    echo '<li class="twitter-list__item">' . $tweet->text . '</li>';
+  }
+  echo '</ul>';
+  ?>
+  <hr/>
+  <div class="contact-buttons">
+    <a href="http://oi.vresp.com/?fid=9fcbd66d02" title="Subscribe to Newsletter" class="header-title contact-button contact-button--newsletter">Subscribe to Newsletter</a>
+    <a href="<?php echo home_url('/'); ?>?feed=rss2" class="header-title contact-button contact-button--rss">Subscribe to Blog</a>
+  </div>
 </aside><!-- end main-aside -->
